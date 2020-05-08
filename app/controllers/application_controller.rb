23a4cab21875a1cec6091662dd9230da70ab52d7
Bruno Toledo <brunogalizzi@gmail.com>
2020-05-07 22:09:53 -0300

    Search categories defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_types
   include Pundit
 
   # Pundit: white-list approach.
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def set_types
+    @types = Type.all
   end
 end
