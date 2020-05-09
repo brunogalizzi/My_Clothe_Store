2d826282dc879e87c57e5f5c99cfd826cc4753aa
Bruno Toledo <brunogalizzi@gmail.com>
2020-05-09 15:37:10 -0300

    Bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
